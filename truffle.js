c2fd22b00017147ddaa49d228111a391ccff6e45
canepat <tullio@deepit.ch>
2018-12-10 01:05:32 +0100

    Add missing Truffle configuration

--- truffle.js
+++ truffle.js
@@ -13,6 +13,28 @@
  */
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*",  // Match any network id
+      gas: 4712388
+    }
+  },
+  coverage: {
+    host: "localhost",
+    network_id: "*",
+    port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
+    gas: 4712388,       // <-- Use this high gas value
+    gasPrice: 100000    // <-- Use this low gas price
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
+  },
+  mocha: {
+    enableTimeouts: false
+  }
 };
